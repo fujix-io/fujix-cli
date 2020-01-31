07fcceaa724d07f502511cbf497a777838eaf8d1
fania <fania@milkandcartoons.com>
2020-01-31 12:55:31 +0300

    Update necessary packages versions

--- src/generator/install-packages.ts
+++ src/generator/install-packages.ts
@@ -1,7 +1,7 @@
 import { exec } from 'child_process';
 import { MethodOptions, ROOT_DIR } from '.';
 
-export const packages = ['graphql', 'graphql-tools', 'graphql-binding', 'node-fetch', 'apollo-link-http', 'dotenv'];
+export const packages = ['graphql@"^0.11.0 || ^0.12.0 || ^0.13.0 || ^14.0.0"', 'graphql-tools', 'graphql-binding@2.5.2', 'node-fetch', 'apollo-link-http', 'dotenv'];
 
 const installPackages = async (options: MethodOptions) => {
   let packageManagerPrefix = 'yarn init -y && yarn add';
