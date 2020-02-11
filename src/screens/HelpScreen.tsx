eade964059892cc155d256f1f82d94dc7967e1a5
fania <fania@milkandcartoons.com>
2020-02-11 18:24:29 +0300

    Update HelpScreen

--- src/screens/HelpScreen.tsx
+++ src/screens/HelpScreen.tsx
@@ -48,17 +48,23 @@ const helps: HelpSection[] = [
     commands: [
       {
         command: <React.Fragment>
-          <Color hex={colors.success}>--out</Color>
+          <Color hex={colors.success}>--out/-o</Color>
           <Color hex={colors.green}>{'<folder_name>'}</Color>
         </React.Fragment>,
         description: 'Specify generated folder name',
       },
       {
         command: <React.Fragment>
-          <Color hex={colors.success}>--language </Color>
+          <Color hex={colors.success}>--language/-l</Color>
           <Color hex={colors.green}>{'<typescript|javascript>'}</Color>
         </React.Fragment>,
         description: 'Specify language for generated code',
+      },
+      {
+        command: <React.Fragment>
+          <Color hex={colors.success}>--version/-v</Color>
+        </React.Fragment>,
+        description: 'Show version of package',
       },
       {
         command: '--npm',
