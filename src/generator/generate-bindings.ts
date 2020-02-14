1e638880364e24fd6f08084924201405c8e19bb3
fania <fania@milkandcartoons.com>
2020-02-14 18:21:24 +0300

    Update generate-bindings.ts

--- src/generator/generate-bindings.ts
+++ src/generator/generate-bindings.ts
@@ -3,8 +3,8 @@ import { generateClient } from '@fujix/client';
 import { GENERATED_DIR_PATH, MethodOptions } from '.';
 
 const generateBindings = async (options: MethodOptions) => {
-  const authorization = options.token;
-  const url = options.url;
+  const authorization = process.env.FUJIX_ROOT_TOKEN;
+  const url = process.env.FUJIX_PROJECT_URL;
 
   await generateClient({
     authorization,
